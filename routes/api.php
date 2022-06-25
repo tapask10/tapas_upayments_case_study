fcd2c4ce84e1ae39e8418dad886bf875324aff1b
Tapas Karmakar <tapask10@gmail.com>
2022-06-25 18:42:15 +0530

    move category, products api to unauthorised access

--- routes/api.php
+++ routes/api.php
@@ -4,17 +4,14 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::group(['prefix'=>'v1'], function () {
+    //Cart
     Route::post('login', 'App\Http\Controllers\api\v1\AuthController@login')->middleware(['throttle:5,1']);
-
     //Cart
     Route::post('cart', 'App\Http\Controllers\api\v1\CartController@save');
     Route::put('cart/{id}', 'App\Http\Controllers\api\v1\CartController@update');
     Route::delete('cart/{id}', 'App\Http\Controllers\api\v1\CartController@delete');
     Route::get('cart', 'App\Http\Controllers\api\v1\CartController@list');
-});
-
-
-Route::group(['prefix' => 'v1', 'middleware' => 'auth:api'], function () {
+    //Category
     Route::get('categories', 'App\Http\Controllers\api\v1\CategoryController@list');
     //Products
     Route::post('products', 'App\Http\Controllers\api\v1\ProductController@save');
